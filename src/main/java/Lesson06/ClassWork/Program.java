3ea7825f3f39edbd30f31a9a9f1676bff874f6d7
PSV <pavshin@mail.ru>
2022-08-14 13:29:58 +0700

    Подготовился к семинару 6

---
+++ src/main/java/Lesson06/ClassWork/Program.java
@@ -0,0 +1,7 @@
+package Lesson06.ClassWork;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Lesson06/HomeWork/Program.java
@@ -0,0 +1,7 @@
+package Lesson06.HomeWork;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
