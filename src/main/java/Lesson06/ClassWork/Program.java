909cc5713455fda936e36e4100112bce0cd27a8b
PSV <pavshin@mail.ru>
2022-08-25 02:47:37 +0700

    classwork is finished

--- src/main/java/Lesson06/ClassWork/Program.java
+++ src/main/java/Lesson06/ClassWork/Program.java
@@ -2,6 +2,12 @@ package Lesson06.ClassWork;
 
 public class Program {
     public static void main(String[] args) {
+        Validator validator = new Validator();
+        while (true) {
+            System.out.println("Input what type of shape do you want to add:\n" +
+                    "1. CIRCLE; 2. SQUARE, 3. RECTANGLE; 4. TRIANGLE, 5. ELLIPSE, 6. Show me the list");
+            validator.type();
+        }
 
     }
 }
