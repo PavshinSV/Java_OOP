dfe411d64d2de47dba1b9e1f520da13a384e8c5a
PSV <pavshin@mail.ru>
2022-08-28 12:20:58 +0700

    README Lesson01

--- src/main/java/org/lesson01/Person.java
+++ src/main/java/org/lesson01/Person.java
@@ -1,11 +1,18 @@
 package org.lesson01;
 
+/**
+ * Класс описывающий Человека
+ * с полями <b>fName</b> - имя, <b>sName</b> - фамилия, <b>bData</b> - отчество, <b>gender</b> - дата рождения
+ */
 public class Person {
     private final String fName;
     private final String sName;
     private final String bDate;
     private final Gender gender;
 
+    /**
+     * Конструктор - создание нового объекта
+     */
     public Person(String firstName, String surName, String birthDate, Gender gender) {
         this.fName = firstName;
         this.sName = surName;
@@ -29,6 +36,9 @@ public class Person {
         return this.gender;
     }
 
+    /**
+     * Переопределение вывода экземпляра на печать в виде "Имя Фамилия Дата рождения Пол"
+     */
     @Override
     public String toString() {
         return String.format("%s %s %s %s", fName, sName, bDate, gender);
